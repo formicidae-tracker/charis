a05907d7beaca014bf54fc5f90856b97795dbf32
Alexandre Tuleu <alexandre.tuleu.2005@polytechnique.org>
2023-12-13 14:46:50 +0100

    Implements a once keyword.

--- src/fort/utils/Defer.hpp
+++ src/fort/utils/Defer.hpp
@@ -2,23 +2,25 @@
 
 #define fu_DEFER_UNIQUE_NAME_INNER(a, b) a##b
 #define fu_DEFER_UNIQUE_NAME(base, line) fu_DEFER_UNIQUE_NAME_INNER(base, line)
-#define fu_DEFER_NAME fu_DEFER_UNIQUE_NAME(zz_defer, __LINE__)
-#define defer auto fu_DEFER_NAME = fort::utils::details::Defer_void{} *[&]()
+#define fu_DEFER_NAME                    fu_DEFER_UNIQUE_NAME(zz_defer, __LINE__)
+#define defer                            auto fu_DEFER_NAME = fort::utils::details::Defer_void{} *[&]()
 
 namespace fort {
 namespace utils {
 namespace details {
 template <typename Lambda> struct Deferrer {
-  Lambda lambda;
+	Lambda lambda;
 
-  ~Deferrer() { lambda(); };
+	~Deferrer() {
+		lambda();
+	};
 };
 
 struct Defer_void {};
 
 template <typename Lambda>
 Deferrer<Lambda> operator*(Defer_void, Lambda &&lambda) {
-  return {lambda};
+	return {lambda};
 }
 
 } // namespace details
---
+++ src/fort/utils/Once.hpp
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <utility>
+
+#define fu_ONCE_UNIQUE_NAME_INNER(a, b) a##b
+#define fu_ONCE_UNIQUE_NAME(base, line) fu_ONCE_UNIQUE_NAME_INNER(base, line)
+#define fu_ONCE_NAME                    fu_ONCE_UNIQUE_NAME(zz_once, __LINE__)
+#define once                            static auto fu_ONCE_NAME = fort::utils::details::Once_void{} *[&]()
+
+namespace fort {
+namespace utils {
+namespace details {
+template <typename Lambda> struct Oncer {
+	Oncer(Lambda &&lambda) {
+		std::forward<Lambda>(lambda)();
+	}
+};
+
+struct Oncer_void {};
+
+template <typename Lambda>
+Oncer<Lambda> operator*(Oncer_void, Lambda &&lambda) {
+	return {std::forward<Lambda>(lambda)};
+}
+
+} // namespace details
+} // namespace utils
+} // namespace fort
